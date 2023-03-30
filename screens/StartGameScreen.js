55248c20e5de8551d87c545253060228be52659b
JoeIt <josignaciot@gmail.com>
2023-03-30 17:55:17 -0300

    Styling for Android & iOS

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,9 +1,10 @@
 import { TextInput, View } from 'react-native';
+import { StyleSheet } from 'react-native-web';
 import PrimaryButton from '../components/PrimaryButton';
 
-function StartOverScreen() {
+function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -11,4 +12,19 @@ function StartOverScreen() {
   );
 }
 
-export default StartOverScreen;
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: '#72063c',
+    borderRadius: 8,
+    elevation: 8,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
